ff34530032c322f47ddb1f3e9e233e4eda81de59
Левыкин Алексей Александрович <alevykin@alfabank.ru>
2025-06-17 11:01:58 +0500

    Валидация пароля

--- src/main/java/ru/netology/HW_REST/controller/AuthorizationController.java
+++ src/main/java/ru/netology/HW_REST/controller/AuthorizationController.java
@@ -1,5 +1,9 @@
 package ru.netology.HW_REST.controller;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +23,15 @@ public class AuthorizationController {
     }
 
     @GetMapping("/authorize")
-    public List<Authorities> getAuthorities(@RequestParam("user") String user, @RequestParam("password") String password) {
+    public List<Authorities> getAuthorities(
+            @RequestParam("user")
+            String user,
+            @RequestParam("password")
+            @Validated
+            @NotBlank
+            @Length(min = 8)
+            @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[_!?@#$%^&+=])(?=\\S+$).{8,}$")
+            String password) {
         return service.getAuthorities(user, password);
     }
 }
